2bc914bbb654af694331a23da3967f57a7e39369
synnz <escape6661@gmail.com>
2017-01-29 23:51:56 +0700

    finish ch5

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,16 @@ RSpec.feature "User pages" do
 
   subject { page }
 
+  shared_examples_for 'signup page' do
+    it { should have_selector('h1', text: content) }
+    it { should have_title(full_title(title)) }
+  end
+
   describe "signup page" do
     before { visit signup_path }
+    let(:content) { 'Sign up' }
+    let(:title) { 'Sign up' }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    it_should_behave_like'signup page'
   end
 end
